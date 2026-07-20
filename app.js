20ea77db0f7374f526514a84cb3fed36f8b28788
Ucokike18 <okikeuchennac@gmail.com>
2026-07-20 06:51:34 +0100

    refactored code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//DOM Elements
+
 const taskInput = document.getElementById("taskInput");
 
 const addTaskBtn = document.getElementById("addTaskBtn");
@@ -18,7 +20,11 @@ const darkBtn = document.getElementById("darkBtn");
 
 const body = document.body;
 
+//State
+
 let tasks = [];
+
+//Functions
 
 function renderTasks() {
   taskList.innerHTML = "";
@@ -32,10 +38,7 @@ function renderTasks() {
     const deleteBtn = document.createElement("button");
 
     deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener("click", () => {
-        tasks.splice(index, 1);
-        renderTasks();
-    });
+    deleteBtn.addEventListener("click", () => deleteTask(index));
 
     li.appendChild(deleteBtn);
 
@@ -45,41 +48,77 @@ function renderTasks() {
 
 }
 
-settingsBtn.addEventListener("click", ()=> {
+function openModal() {
   overlay.classList.remove("hidden");
   overlay.classList.add("show");
-});
+}
 
-darkBtn.addEventListener("click", () => {
-  body.classList.add("dark-theme");
-});
-
-lightBtn.addEventListener("click", () => {
-  body.classList.remove("dark-theme")
-})
-
-closeBtn.addEventListener("click", () => {
+function closeModal () {
   overlay.classList.add("hidden");
-  overlay.classList.remove("show")
-})
+  overlay.classList.remove("show");
+}
 
-taskInput.addEventListener("input", () => {
-  errorMessage.classList.add("hidden");
-});
+function enableDarkTheme () {
+  body.classList.add("dark-theme");
+}
 
-addTaskBtn.addEventListener("click", () => {
+function enableLightTheme (){
+  body.classList.remove("dark-theme");
+}
 
-  const taskValue = taskInput.value.trim();
-
-  if (taskValue === "") {
-    errorMessage.classList.remove("hidden");
-    return
+function validateTask (taskValue){
+   if (taskValue === "") {
+    return false;
   }
+  return true;
+}
 
+function showError () {
+  errorMessage.classList.remove("hidden");
+}
+
+function hideError () {
+  errorMessage.classList.add("hidden");
+}
+
+function addTask (taskValue) {
   tasks.push(taskValue);
   
-
   renderTasks();
 
   taskInput.value = "";
+  
+  taskInput.focus();
+}
+
+function deleteTask (index) {
+  tasks.splice(index, 1);
+  renderTasks();
+}
+
+//Event Listeners
+
+settingsBtn.addEventListener("click", openModal);
+
+darkBtn.addEventListener("click", enableDarkTheme);
+
+lightBtn.addEventListener("click", enableLightTheme);
+
+closeBtn.addEventListener("click", closeModal);
+
+taskInput.addEventListener("input", hideError);
+
+addTaskBtn.addEventListener("click", () => {
+
+ const taskValue = taskInput.value.trim();
+
+if (!validateTask(taskValue)) {
+
+  showError();
+
+  return;
+}
+
+addTask(taskValue);
+  
 });
